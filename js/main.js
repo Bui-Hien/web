b9731ec920489b153a16c39cae1fa5abf5164213
Hiền Bùi <buixuanhienmy@gmail.com>
2023-04-25 11:10:16 +0700

    fixMenu

--- js/main.js
+++ js/main.js
@@ -54,7 +54,6 @@ function hien2() {
 }
 
 
-
 //lây kich thươc cua cua so dang mo
 var w = window.innerWidth;
 var h = window.innerHeight;
@@ -173,48 +172,99 @@ dragLink.addEventListener('dragend', function () {
 
 //hien menu table, mebile
 function displayMenu() {
+    if(w<1024){
 
-    var element = document.getElementsByClassName("header-center");
-    if (element[0].classList.contains("header-center_block")) {
-        const element = document.querySelectorAll('.header-center_block')[0];
+    }
+        var element = document.getElementsByClassName("header-center");
+        if (element[0].classList.contains("header-center_block")) {
+            const element = document.querySelectorAll('.header-center_block')[0];
 
-        // Gán tên animation và thời gian hoàn thành của animation cho phần tử
-        element.style.animationName = 'example';
-        element.style.animationDuration = '1s';
+            // Gán tên animation và thời gian hoàn thành của animation cho phần tử
+            element.style.animationName = 'example';
+            element.style.animationDuration = '1s';
 
-        // Định nghĩa keyframes cho animation
-        const styleSheet = document.createElement('style');
-        styleSheet.innerHTML = `
+            // Định nghĩa keyframes cho animation
+            const styleSheet = document.createElement('style');
+            styleSheet.innerHTML = `
       @keyframes example {
         from {margin-left: 0;}
         to {margin-left: -100%;}
       }
     `;
-        document.head.appendChild(styleSheet);
-        setTimeout(() => {
-            element.classList.remove("header-center_block");
-        }, 1000);
+            document.head.appendChild(styleSheet);
+            setTimeout(() => {
+                element.classList.remove("header-center_block");
+            }, 1000);
 
 
-    } else {
-        element[0].classList.add("header-center_block");
-        // Đặt các thuộc tính animation cho phần tử
-        element[0].style.animationName = 'example1';
-        element[0].style.animationDuration = '1s';
+        } else {
+            element[0].classList.add("header-center_block");
+            // Đặt các thuộc tính animation cho phần tử
+            element[0].style.animationName = 'example1';
+            element[0].style.animationDuration = '1s';
 
-        // Định nghĩa keyframes cho animation
-        const styleSheet = document.createElement('style');
-        styleSheet.innerHTML = `
+            // Định nghĩa keyframes cho animation
+            const styleSheet = document.createElement('style');
+            styleSheet.innerHTML = `
           @keyframes example1 {
             from {margin-left: -100%;}
             to {margin-left: 0;}
           }
         `;
-        document.head.appendChild(styleSheet);
-    }
+            document.head.appendChild(styleSheet);
+        }
 }
-function displayMenuTB(){
-    if(w<1024){
+
+function displayMenuTB() {
+    if (w < 1024) {
         displayMenu();
     }
+}
+
+
+// Lấy tất cả các phần tử div trên trang
+const divs = document.querySelectorAll('div');
+
+// Lặp qua từng phần tử div
+divs.forEach(div => {
+    // Kiểm tra nếu id của div trùng với href của menu
+    const divId = div.getAttribute('id');
+    const menu = document.querySelector(`a[href="#${divId}"]`);
+    if (menu) {
+        // Nếu tìm thấy menu, thêm sự kiện lắng nghe khi trang được lăn
+        window.addEventListener('scroll', () => {
+            // Nếu vị trí của div hiện tại trùng với vị trí trên trang
+            if (div.getBoundingClientRect().top < window.innerHeight * 0.5) {
+                // Đánh dấu menu hiện tại
+                document.querySelectorAll('.header-center_item').forEach(item => item.classList.remove('active'));
+                menu.parentElement.classList.add('active');
+            }
+        });
+    }
+});
+
+
+window.onscroll = function () {
+    scrollFunction()
+};
+var headerWraper = document.querySelector('.header-wraprer');
+var headerConten = document.querySelector('.header');
+
+function scrollFunction() {
+    if (document.body.scrollTop > 74 || document.documentElement.scrollTop > 74) {
+        headerWraper.style.background = "#FEFEFE";
+        headerWraper.style.top = "-74px";
+        headerWraper.style.transition = "top 0.5s";
+        headerWraper.style.top = "0";
+        headerWraper.style.position = "fixed";
+        headerConten.style.background = "#FEFEFE";
+
+    } else {
+        headerWraper.style.transition = "top 0.5s";
+        headerWraper.style.top = "-74px";
+        headerWraper.style.removeProperty("position");
+        headerWraper.style.background = "#f6f7f9";
+        headerConten.style.background = "#f6f7f9";
+    }
+
 }
